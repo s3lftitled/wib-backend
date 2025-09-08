82b12c675d4ee027d6a43f7a7840dc19884498e7
s3lftitled <jjaviendemonteverde@gmail.com>
2025-09-08 16:16:12 +0800

    feat(auth): add refreshAccessToken to issue new access tokens

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const errorHandler = require('./src/middlewares/errorHandler') // Global error h
 const authRouter = require('./src/routers/authRouter')
 const employeeRouter = require('./src/routers/employeeRouter')
 const adminRouter = require('./src/routers/adminRouter')
+const tokenRouter = require('./src/routers/tokenRouter')
 
 // Initialize the Express app
 const app = express()
@@ -38,6 +39,9 @@ app.use('/api/employee', employeeRouter)
 
 // Admin base routes
 app.use('/api/admin', adminRouter)
+
+// Token base routes
+app.use('/api/token', tokenRouter)
 
 // Global error-handling middleware
 app.use(errorHandler)
